b6736f4c5139348b9f7b0715da82b06e9b14a0bc
nuravvarun <sachdevavarun753@gmail.com>
2018-04-04 23:01:05 +0530

    adding intial files

---
+++ src/pages/tutorial/tutorial.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
+
+import { TranslateService } from '@ngx-translate/core';
+
+
+
+@IonicPage()
+@Component({
+  selector: 'page-tutorial',
+  templateUrl: 'tutorial.html'
+})
+export class TutorialPage {
+  dir: string = 'ltr';
+  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService, public platform: Platform)
+   {
+    this.dir = platform.dir();
+    
+  }
+
+  startApp() {   
+    this.navCtrl.setRoot('WelcomePage', {}, {
+      animate: true,
+      direction: 'forward'
+    });
+  }
+  startApp2() { 
+    this.navCtrl.setRoot('LoginPage', {}, {
+      animate: true,
+      direction: 'forward'
+    });
+  }
+  ionViewDidEnter() {
+    // the root left menu should be disabled on the tutorial page
+    this.menu.enable(false);
+  }
+
+  ionViewWillLeave() {
+    // enable the root left menu when leaving the tutorial page
+    this.menu.enable(true);
+  }
+
+}
