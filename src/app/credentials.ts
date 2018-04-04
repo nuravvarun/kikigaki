fe9d550b7af9d7469d63b537ab694d06d06a41f2
nuravvarun <sachdevavarun753@gmail.com>
2018-04-05 00:09:45 +0530

    adding files

---
+++ src/app/credentials.ts
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+ export var config = {
+    apiKey: "AIzaSyCYK_GRsKygHXQGXbPpPFUHC-XrAWBfMoM",
+    authDomain: "kikigakiaudiosdb.firebaseapp.com",
+    databaseURL: "https://kikigakiaudiosdb.firebaseio.com",
+    projectId: "kikigakiaudiosdb",
+    storageBucket: "kikigakiaudiosdb.appspot.com",
+    messagingSenderId: "506828991311"
+  };
+  
